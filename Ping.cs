112abb03c45fadb3c804c5d30250aac95c8745c4
Farhat Saleem <41129835+fsaleemm@users.noreply.github.com>
2021-04-13 16:41:28 -0400

    updated function

--- Ping.cs
+++ Ping.cs
@@ -26,7 +26,7 @@ namespace FS.Demo
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "Responding to your Ping with Pong! It works!!"
+                ? "Responding to your Ping with Pong! Ping Pong Hahah!!"
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
